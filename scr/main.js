607b3f8dbc5699b8ce6d53a0a7baa932b5ebee35
Patyczakus <patryk.topolski0707@gmail.com>
2021-10-01 17:39:31 +0200

    Edycja #3
    Dodanie "main.js" oraz podłączenie do stron. Zmiana także "goto()" na "go()".

---
+++ scr/main.js
@@ -0,0 +1,11 @@
+function go(anch) {
+    window.location.href = anch + ".html";
+}
+function select_all(obj) {
+    var text_val=eval(obj);
+    text_val.focus();
+    text_val.select();
+    if (!document.all) return; // IE only
+    r = text_val.createTextRange();
+    r.execCommand('copy');
+}

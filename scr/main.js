3ebf18347fcc8704090936c1746ec56e7942d1cd
Patyczakus <patryk.topolski0707@gmail.com>
2021-10-30 11:06:22 +0200

    Edycja
    Naprawienie problemu

--- scr/main.js
+++ scr/main.js
@@ -7,12 +7,3 @@ function start() {
 function go(anch) {
     window.location.href = anch + ".html";
 }
-
-function select_all(obj) {
-    var text_val=eval(obj);
-    text_val.focus();
-    text_val.select();
-    if (!document) return; // IE only
-    r = text_val.createTextRange();
-    r.execCommand('copy');
-}

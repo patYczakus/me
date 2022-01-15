376929f478134a9f7a8dee6a750d0245fb91c233
Patyczakus <patryk.topolski0707@gmail.com>
2022-01-15 15:12:26 +0100

    Edycja styli
    > :active zamiast :focus w przycisu RP.G.
    > Ulepszony "Poput"
    > Custom Scrollbar
    > usunięcie write()

--- scr/codes/js/main.js
+++ scr/codes/js/main.js
@@ -4,16 +4,3 @@ function go(anch) {
     else
         window.location.href = anch + ".html";
 };
-
-function write(where, text, at_time) {
-    var text1 = ""
-    var x = 0
-    for (i=0; i<text.lenght; i++) {
-        x++
-        setTimeout(function() {
-            text1 = text1 + text.charAt(i)
-            document.getElementById(where).innerText = text1
-        }, x*at_time)
-    }
-};
-

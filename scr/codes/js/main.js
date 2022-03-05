e7fcc3e9bf505d717901b0ddf8a76e8e644efb77
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2022-03-05 10:25:30 +0100

    Budowa wersji 3.0

--- scr/codes/js/main.js
+++
@@ -1,6 +0,0 @@
-function go(anch) {
-    if (anch.startsWith("https://") || anch.startsWith("http://"))
-        window.open(anch, `target="_blank"`)
-    else
-        window.location.href = anch + ".html";
-};

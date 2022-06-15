5c2d0d777d1bbd4e82de118bb1225e7a6d37ff03
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2022-06-15 14:30:37 +0200

    Mała edycja

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,25 +21,27 @@ const set = {
 window.onload = checkIframe()
 
 function checkMobileDevice() {
-    if (window.innerWidth+150 <= window.innerHeight) {
-        document.body.classList.add('phone');
-        document.getElementById("nav").innerHTML = `<button onclick="showNav()" class="more onlyBack">⋅⋅⋅</button>`
+    setTimeout(() => {
+        if (window.innerWidth+150 <= window.innerHeight) {
+            document.body.classList.add('phone');
+            document.getElementById("nav").innerHTML = `<button onclick="showNav()" class="more onlyBack">⋅⋅⋅</button>`
 
-        set.navBox(0)
-    } else {
-        text = ""
-        iSys = 0
-        for (i=0; i<navigateData.length && i<3; i++) {
-            if (!window.location.pathname.endsWith(navigateData[iSys].href)) text += `<button onclick="window.open('${navigateData[iSys].href}', '_self')">${navigateData[iSys].name}</button>`
-            else text += `<button class="thisPage onlyBack">${navigateData[iSys].name}</button>`
-            iSys++
+            set.navBox(0)
+        } else {
+            text = ""
+            iSys = 0
+            for (i=0; i<navigateData.length && i<3; i++) {
+                if (!window.location.pathname.endsWith(navigateData[iSys].href)) text += `<button onclick="window.open('${navigateData[iSys].href}', '_self')">${navigateData[iSys].name}</button>`
+                else text += `<button class="thisPage onlyBack">${navigateData[iSys].name}</button>`
+                iSys++
+            }
+            if (iSys < navigateData.length) text += `<button onclick="showNav()" class="more onlyBack">⋅⋅⋅</button>`
+            document.getElementById("nav").innerHTML = text
+            if (iSys < navigateData.length) set.navBox(5)
         }
-        if (iSys < navigateData.length) text += `<button onclick="showNav()" class="more onlyBack">⋅⋅⋅</button>`
-        document.getElementById("nav").innerHTML = text
-        if (iSys < navigateData.length) set.navBox(5)
-    }
 
-    document.getElementById("banner").style.marginLeft = "-2000vw"
+        document.getElementById("banner").style.marginLeft = "-2000vw"
+    }, 100)
 }
 
 function showNav() {
@@ -51,24 +53,6 @@ function hideNav()  {
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-function scrollProgress() {
-    const totalheight_Page = document.getElementById("content").scrollHeight
-    const currentDistanceFromTop = document.getElementById("content").scrollTop
-
-    const windowHeight = document.getElementById("content").clientHeight
-
-    let scrolledPrestange = (currentDistanceFromTop / (totalheight_Page - windowHeight)) * 100
-
-    document.getElementById("progress_scroll").style.width = `${Math.round(scrolledPrestange)}%`
-}
-
-function proces() {
-    scrollProgress()
-    setTimeout(function(){
-        proces()
-    }, 10)
-}
 
 function openAtNewWindow(adres, nazwa, szerokosc, wysokosc, center = Boolean(false)) {
     var fullScreen = false;
@@ -100,24 +84,6 @@ function checkIframe() {
         alert(`Blokada przeciw iframemem!\n\nWykryto korzystanie z osadzenia - ta blokada służy do blokowania zawartości z osadzenia, ponieważ się ona poprawnie się nie wyświetli. Aby temu zapobiec, włącz stronę normalnie.`)
         setTimeout(() => { document.body.innerHTML = `<div id="banner" class><a onclick="window.parent.location.href = window.location.href">Idź do strony</a></div>` }, 500)
     } else {
-        setTimeout(function(){
-            try {
-                checkMobileDevice()
-                proces()
-            } catch (err) {
-                try {
-                    checkMobileDevice()
-                    proces()
-                } catch (err) {
-                    try {
-                        checkMobileDevice()
-                        proces()
-                    } catch (err) {
-                        console.error(err)
-                        checkMobileDevice()
-                    }
-                }
-            }
-        }, 500)
+        checkMobileDevice()
     }
 }

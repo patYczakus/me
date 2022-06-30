95f9bcd59653ce05c40179bd8b422d86bb5683c0
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2022-06-30 09:27:52 +0200

    Update dowcipy.js

--- src/scripts/dowcipy.js
+++ src/scripts/dowcipy.js
@@ -19,11 +19,19 @@ const dowcipy = [
     },
     {
         isJSON: true, 
+        index: { "qu": "Jak nazywa się rosyjskie małżeństwo?", "an": "Związek Radziecki" }
+    },
+    {
+        isJSON: true, 
         index: { "qu": "Co mówi Ukrainka, kiedy chce użądlić ją pszczoła?", "an": "Nie, pani Maju" }
     },
     {
         isJSON: false, 
         index: "Dwóm przyjaciółkom po długich namowach, udało się wreszcie przekonać mężów, żeby zostali w domu, a one same wychodzą na kolację do knajpy, żeby przypomnieć sobie dawne czasy.<br />Po zabawnie spędzonym wieczorze, dwóch butelkach białego wina, szampanie i buteleczce wódki opuszczają restaurację całkowicie pijane!<br />W drodze powrotnej obie nachodzi nagła potrzeba, może dlatego że dużo wypiły.<br />Nie wiedząc gdzie iść się wysikać, jedna mówi do drugiej:<br />- Wejdźmy na ten cmentarz, tutaj na pewno nikogo nie będzie.<br />Wchodzą na cmentarz, najpierw jedna ściąga majtki, sika, wyciera się tymi majtkami i oczywiście je wyrzuca.<br />Widząc to, druga od razu sobie przypomina, ze ma na sobie drogą markową bieliznę od męża i szkoda by ją wyrzucić.<br />Ściąga więc majtki, wkłada je do kieszeni, sika i zrywa kokardę z pierwszego lepszego wieńca, żeby się podetrzeć.<br />Na drugi dzień mąż pierwszej, dzwoni do męża drugiej:<br />- Jurek, nawet sobie nie wyobrażasz co się stało! Koniec mojego małżeństwa!<br />- Dlaczego?<br />- Moja żona wróciła o 5 rano kompletnie pijana i na dodatek bez majtek! Od razu wywaliłem ją z domu.<br /> - Marek, to jeszcze nic, wiesz co wykombinowała moja! Nie tylko przyszła pijana i bez majtek, ale miała włożoną w tyłek czerwoną kokardę z napisem: NIGDY CIĘ NIE ZAPOMNIMY, ŁUKASZ, IGNAŚ, STASIEK I POZOSTALI PRZYJACIELE Z SIŁOWNI."
+    },
+    {
+        isJSON: false, 
+        index: "[Pielęgniarka] Nie mogę Cię leczyć, Kasiu...<br />[Kasia] Czemu?<br />[P] Bo ja jestem lekarzem rodzinnym, a ty sierotą."
     },
 ]
 
